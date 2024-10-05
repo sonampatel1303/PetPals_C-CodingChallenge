848e9c998731bf24ff84665b9ce203932f230f31
Sonam Patel <96812227+sonampatel1303@users.noreply.github.com>
2024-10-05 13:25:29 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using Petpals.MainModule;
+
+namespace Petpals
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+          MainMenu mainMenu = new MainMenu();
+            mainMenu.DisplayMenu();
+        }
+    }
+}
