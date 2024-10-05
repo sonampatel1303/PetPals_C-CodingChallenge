4a9454ecf5f74572d52677c173ad6e201f3e85dc
Sonam Patel <96812227+sonampatel1303@users.noreply.github.com>
2024-10-05 13:24:53 +0530

    Add files via upload

---
+++ PetException.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+    namespace Petpals.Exceptions
+    {
+        // Custom exception for Invalid Pet Age
+        public class InvalidPetAgeException : Exception
+        {
+            public InvalidPetAgeException():base("Invalide age") { }
+            public InvalidPetAgeException(string message) : base(message) { }
+
+        }
+
+        // Custom exception for Insufficient Funds
+        public class InsufficientFundsException : Exception
+        {
+        public InsufficientFundsException() : base("Enter valid funds") { }
+        public InsufficientFundsException(string message) : base(message) { }
+        }
+
+        // Custom exception for Null Reference
+        public class NullReferencePetException : Exception
+        {
+        public NullReferencePetException() : base("Can't access values which are null") { }
+        public NullReferencePetException(string message) : base(message) { }
+    }
+
+        // Custom exception for File Handling
+        public class FileHandlingException : Exception
+        {
+            public FileHandlingException(string message) : base(message) { }
+        }
+    }
+
+
